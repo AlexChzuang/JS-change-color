9b6dcdddd89d059fe53b2fa02fea4e57705a8ad9
Alex Chuang <79676142+AlexChzuang@users.noreply.github.com>
2021-04-20 10:14:19 +0800

    classList.toggle

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('button').addEventListener('onclick', function () {
-    var element = document.querySelector('circle')
-    element.classList.toggle('active');
+  document.querySelector('button').addEventListener('click', function () {
+
+    var element = document.querySelector('.circle')  // 因為這裡是要選 class 所以前面要 .
+    element.classList.toggle('active'); // 因為前面有 classList 所以選 class 的話不用給它 .
   })
 })
+
